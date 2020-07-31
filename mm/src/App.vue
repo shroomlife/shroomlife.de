ade62cdd4e0d01a3b65e5363e8f2b106af2e167e
robin <robin@shroomlife.de>
2020-07-31 03:21:24 +0200

    added mm vue app

---
+++ mm/src/App.vue
@@ -0,0 +1,142 @@
+<template>
+  <div id="app">
+    <form v-if="!loggedIn" @submit.prevent="doLogin">
+      <div class="form-group">
+        <input type="password" class="form-control" placeholder="Password" v-model="login.password" />
+      </div>
+      <button type="submit" class="btn btn-primary btn-block">Anmelden</button>
+    </form>
+    <div v-else>
+      <div v-if="isLoading">
+        <div class="d-flex">
+          <p>Daten werden geladen ...</p>
+        </div>
+      </div>
+      <div v-else>
+        <div class="list-group text-left">
+          <a v-for="contact in contacts" :key="contact.name" :href="getPhoneLink(contact)" class="list-group-item list-group-item-action">
+            <div class="row">
+              <div class="col-5">
+                <button class="btn btn-success"><MdCallIcon /> Anrufen</button>
+              </div>
+              <div class="col">
+                <div><h3>{{contact.name}}</h3></div>
+                <div>{{contact.phone}}</div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-danger btn-block mt-3">Ausloggen</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import MdCallIcon from 'vue-ionicons/dist/md-call.vue'
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      login: {
+        password: "",
+        token: false,
+      },
+      isLoading: true,
+      contacts: [],
+    };
+  },
+  components: {
+    MdCallIcon
+  },
+  computed: {
+    loggedIn() {
+      return String(this.login.token).length === 255;
+    },
+  },
+  methods: {
+    getPhoneLink(contact) {
+      return `tel:${contact.phone}`
+    },
+    reset() {
+      this.login.token = false;
+      this.login.password = "";
+    },
+    showException() {
+      this.$swal(
+        "Unbekannter Fehler",
+        "Es gab einen unbekannten Fehler!",
+        "error"
+      );
+      this.reset();
+    },
+    doLogin() {
+      const loginRequestConfig = {
+        method: "post",
+        data: { ...this.login },
+        url: "//localhost/mm",
+      };
+      axios(loginRequestConfig)
+        .then((response) => {
+          if (response.data.length === 255) {
+            this.login.token = String(response.data);
+          } else this.showException();
+        })
+        .catch(() => {
+          this.$swal(
+            "Loginfehler",
+            "Du hast ein falsches Passwort eingegeben.",
+            "error"
+          );
+          this.reset();
+        });
+    },
+    loadContacts() {
+      const loginRequestConfig = {
+        method: "post",
+        data: { ...this.login },
+        url: "//localhost/mmA",
+        headers: {
+          MM_KEY: this.login.token,
+        },
+      };
+      axios(loginRequestConfig)
+        .then((response) => {
+          this.contacts = response.data.slice(0);
+          this.isLoading = false
+        })
+        .catch(this.showException);
+    },
+  },
+  watch: {
+    loggedIn(newState) {
+      newState === true && this.loadContacts();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-bottom: 60px;
+}
+a {
+  cursor: pointer;
+}
+h3 {
+  font-weight: bold;
+  margin-bottom: 0;
+}
+div.ion {
+  vertical-align: text-top;
+}
+</style>
---
+++ mm/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+import 'vue-ionicons/ionicons.css'
+Vue.use(VueSweetalert2);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')

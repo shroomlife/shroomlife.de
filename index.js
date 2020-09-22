f059f015c92ded5149b0ffad026f7182be64ada5
shroomlife <robin@shroomlife.de>
2020-09-22 12:51:35 +0200

    added update

--- index.js
+++ index.js
@@ -1,78 +1,71 @@
 require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
-const handlebars = require('handlebars');
-const fs = require('fs');
-const compression = require('compression');
-const http = require('http');
-const fork = require('child_process').fork;
+const express = require('express')
+const bodyParser = require('body-parser')
+const handlebars = require('handlebars')
+const fs = require('fs')
+const compression = require('compression')
+const http = require('http')
+const fork = require('child_process').fork
 const cors = require('cors')
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
-handlebars.registerPartial("include", (context) => {
-	let file = `${__dirname}/views/inc/${context.path}.html`;
-	let html = fs.readFileSync(file).toString();
-	let include = handlebars.compile(html);
-	return include(context);
-});
+handlebars.registerPartial('include', (context) => {
+  const file = `${__dirname}/views/inc/${context.path}.mustache`
+  const html = fs.readFileSync(file).toString()
+  const include = handlebars.compile(html)
+  return include(context)
+})
 
-const configPath = `${__dirname}/config.json`;
+const configPath = `${__dirname}/config.json`
 
-const stage = typeof process.argv[2] !== 'undefined' ? String(process.argv[2]) : "development";
-const production = stage === "production" ? true : false;
+const stage = typeof process.argv[2] !== 'undefined' ? String(process.argv[2]) : 'development'
+const production = stage === 'production'
 
-if(production !== true) {
+if (production !== true) {
   app.use(cors())
 }
 
 app.use(bodyParser.json())
 
-app.use(compression());
+app.use(compression())
 app.get('/', (req, res) => {
+  let indexFilePath = `${__dirname}/public/index.min.html`
+  fs.access(indexFilePath, (exists) => {
+    if (exists && production) {
+      res.sendFile(indexFilePath)
+    } else {
+      const config = loadConfig()
 
-  let indexFilePath = `${__dirname}/public/index.min.html`;
-	fs.exists(indexFilePath, (exists) => {
+      indexFilePath = `${__dirname}/views/index.mustache`
 
-		if (exists && production) {
-			res.sendFile(indexFilePath);
-		} else {
+      fs.readFile(indexFilePath, (err, data) => {
+        if (err) console.error(err)
+        const indexHtml = String(data)
+        const index = handlebars.compile(indexHtml)
 
-			let config = loadConfig();
+        const html = index(config)
+        res.type('html').send(html).end()
+      })
+    }
+  })
+})
 
-			indexFilePath = `${__dirname}/views/index.html`;
-
-			fs.readFile(indexFilePath, (err, data) => {
-
-        let indexHtml = String(data);
-				let index = handlebars.compile(indexHtml);
-
-				let html = index(config);
-				res.type('html').send(html).end();
-
-			});
-
-		}
-
-	});
-
-});
-
-if(production !== true) {
-  const static = express.static(`${__dirname}/static`);
-  app.use(static);
+if (production !== true) {
+  const staticServer = express.static(`${__dirname}/static`)
+  app.use(staticServer)
 }
-const public = express.static(`${__dirname}/public`);
-app.use(public);
+const publicServer = express.static(`${__dirname}/public`)
+app.use(publicServer)
 
-const mmApp = express.static(`${__dirname}/mm/dist`);
-app.use('/mm', mmApp);
+const mmApp = express.static(`${__dirname}/mm/dist`)
+app.use('/mm', mmApp)
 
 app.post('/mm', (req, res) => {
   const password = req.body.password
   console.log('LOGIN ATTEMPT', password, req.headers['x-forwarded-for'] || req.connection.remoteAddress)
-  if(password === process.env.MM_PASSWORD) {
+  if (password === process.env.MM_PASSWORD) {
     console.log('LOGIN SUCCESS')
     res.send(process.env.MM_KEY)
   } else {
@@ -82,64 +75,56 @@ app.post('/mm', (req, res) => {
 })
 
 app.post('/mmA', (req, res) => {
-
-  if(req.headers.mm_key === process.env.MM_KEY) {
+  if (req.headers.mm_key === process.env.MM_KEY) {
     res.json(JSON.parse(process.env.MM_CONTACTS))
   } else {
     res.sendStatus(401)
   }
-
 })
 
 app.get('/:page', (req, res) => {
+  const pagePath = `${__dirname}/views/pages/${req.params.page}.mustache`
+  fs.readFile(pagePath, (err, data) => {
+    if (err) {
+      res.status(404).end()
+    } else {
+      try {
+        const pageHtml = String(data)
+        const page = handlebars.compile(pageHtml)
 
-	let pagePath = `${__dirname}/views/pages/${req.params.page}.html`;
-	fs.readFile(pagePath, (err, data) => {
+        const config = loadConfig()
 
-		if (err) {
-			res.status(404).end();
-		} else {
+        config.page = req.params.page
+        const html = page(config)
 
-			try {
-				let pageHtml = String(data);
-				let page = handlebars.compile(pageHtml);
+        res.type('html').send(html).end()
+      } catch (error) {
+        res.status(500).send(error.message)
+      }
+    }
+  })
+})
 
-				let config = loadConfig();
+const listener = server.listen(80, () => {
+  const address = listener.address()
+  console.log(address)
 
-				config.page = req.params.page;
-				let html = page(config);
+  if (address.address === '::') {
+    console.log(`app is listening at http://localhost:${address.port}`)
+  } else {
+    console.log(`app is listening at http://${address.address}:${address.port}`)
+  }
 
-				res.type('html').send(html).end();
+  if (production) {
+    console.log('starting production build ...')
+    fork('./build.js', [JSON.stringify(loadConfig())])
+  }
+})
 
-			} catch (error) {
-				res.status(500).send(error.message);
-			}
+function loadConfig () {
+  const configString = String(fs.readFileSync(configPath))
+  const config = JSON.parse(configString)
 
-
-		}
-
-	});
-
-});
-
-let listener = server.listen(80, () => {
-
-	let address = listener.address();
-	console.log(`app is listening at ${address.port}`);
-
-	if (production) {
-		console.log("starting production build ...");
-		fork("./build.js", [JSON.stringify(loadConfig())]);
-	}
-
-});
-
-function loadConfig() {
-
-  configString = String(fs.readFileSync(configPath));
-  let config = JSON.parse(configString);
-  
-	config.host = process.env.HOST ? process.env.HOST : (stage === "production" ? "shroomlife.de" : "localhost");
-	return config;
-
+  config.host = process.env.HOST ? process.env.HOST : (stage === 'production' ? 'shroomlife.de' : 'localhost')
+  return config
 }
